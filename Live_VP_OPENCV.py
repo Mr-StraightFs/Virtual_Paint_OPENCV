f4969ed5b1dd294d6c1b5a29326b2a9c91e10900
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-04-13 12:37:05 +0000

    Add color settings and values

--- Live_VP_OPENCV.py
+++ Live_VP_OPENCV.py
@@ -9,6 +9,22 @@ cap.set(3, frameWd)
 cap.set(4, frameHeight)
 cap.set(10,100)           # set up the brightness level
 
+# The following measures are based on Min/Max hue , Min/Max Saturation
+# and Min/Max Value resectively
+# for the specifics of these measures please check my color detection repository :
+Colors = [[5,107,0,19,255,255],
+            [133,56,0,159,156,255],
+            [57,76,0,100,255,255],
+            [90,48,0,118,255,255]]
+# The color RBG Values , respectively :
+ColorValues = [[51,153,255],
+                 [255,0,255],
+                 [0,255,0],
+                 [255,0,0]]
+
+myPoints = []  ## Three dimentional np array with [x , y , colorId ]
+
+
 # Webcam Live Video Capture Loop : (Type q to stop)
 while True:
     success, img = cap.read()
