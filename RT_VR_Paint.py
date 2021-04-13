b16b2176becbc855d11ca3bb7ef8625e4367c96b
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-04-13 12:06:51 +0000

    Video Capture set up

--- RT_VR_Paint.py
+++ RT_VR_Paint.py
@@ -9,3 +9,11 @@ cap.set(3, frameWd)
 cap.set(4, frameHeight)
 cap.set(10,100)           # set up the brightness level
 
+# Webcam Live Video Capture Loop : (Type q to stop)
+while True:
+    success, img = cap.read()
+    if img is None or (cv2.waitKey(1) & 0xFF == ord('q')):
+        break
+    cv2.imshow("Result", img)
+
+
