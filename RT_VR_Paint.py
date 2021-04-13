715ecaa8349763d3c76a37c74c7b964edf93d14c
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-04-13 11:53:00 +0000

    Initial Commit

---
+++ RT_VR_Paint.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+
+# set up the video window frame's size
+frameWd = 640
+frameHeight = 480
+cap = cv2.VideoCapture(0)  # Set u the video capture speed
+cap.set(3, frameWd)
+cap.set(4, frameHeight)
+cap.set(10,100)           # set up the brightness level
+
