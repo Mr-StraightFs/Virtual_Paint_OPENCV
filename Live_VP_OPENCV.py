a16a95c816f08e7e59a04d08fb1ca478747a34da
Night_Owl <81880125+Mr-StraightFs@users.noreply.github.com>
2024-02-21 23:06:36 +0100

    Update Live_VP_OPENCV.py

--- Live_VP_OPENCV.py
+++ Live_VP_OPENCV.py
@@ -1,75 +1,64 @@
 import cv2
 import numpy as np
 
-# set up the video window frame's size
-frameWd = 640
-frameHeight = 480
-cap = cv2.VideoCapture(0)  # Set u the video capture speed
-cap.set(3, frameWd)
-cap.set(4, frameHeight)
-cap.set(10,100)           # set up the brightness level
+class ColorDetector:
+    def __init__(self):
+        self.frame_width = 640
+        self.frame_height = 480
+        self.cap = cv2.VideoCapture(0)
+        self.cap.set(3, self.frame_width)
+        self.cap.set(4, self.frame_height)
+        self.cap.set(10, 100)
+        self.colors = [[5,107,0,19,255,255],
+                       [133,56,0,159,156,255],
+                       [57,76,0,100,255,255],
+                       [90,48,0,118,255,255]]
+        self.color_values = [[51,153,255],
+                             [255,0,255],
+                             [0,255,0],
+                             [255,0,0]]
+        self.points = []
 
-# The following measures are based on Min/Max hue , Min/Max Saturation
-# and Min/Max Value resectively
-# for the specifics of these measures please check my color detection repository :
-Colors = [[5,107,0,19,255,255],
-            [133,56,0,159,156,255],
-            [57,76,0,100,255,255],
-            [90,48,0,118,255,255]]
+    def detect_color(self, img):
+        img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        new_points = []
+        for i, color in enumerate(self.colors):
+            lower = np.array(color[:3])
+            upper = np.array(color[3:])
+            mask = cv2.inRange(img_hsv, lower, upper)
+            x, y = self.get_contours(mask)
+            cv2.circle(img_output, (x, y), 15, self.color_values[i], cv2.FILLED)
+            if x != 0 and y != 0:
+                new_points.append([x, y, i])
+            cv2.imshow(str(color[0]), mask)
+        return new_points
 
-ColorValues = [[51,153,255],  # The color RBG Values
-                 [255,0,255],
-                 [0,255,0],
-                 [255,0,0]]
+    def get_contours(self, img):
+        contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        x, y, w, h = 0, 0, 0, 0
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            if area > 500:
+                cv2.drawContours(img_output, contour, -1, (255, 0, 0), 3)
+                perimeter = cv2.arcLength(contour, True)
+                approx = cv2.approxPolyDP(contour, 0.02 * perimeter, True)
+                x, y, w, h = cv2.boundingRect(approx)
+        return x + w // 2, y
 
-myPoints = []  ## Three dimentional np array with [x , y , colorId ]
+    def draw_on_cam(self):
+        for point in self.points:
+            cv2.circle(img_output, (point[0], point[1]), 10, self.color_values[point[2]], cv2.FILLED)
 
-
-def detectColor(img,Colors,ColorValues):  # Detect Color Function
-    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)  #Converting to the HSV color enviroment
-    count = 0
-    newPoints=[]
-    for clr in Colors:
-        lower = np.array(clr[0:3])
-        upper = np.array(clr[3:6])
-        mask = cv2.inRange(imgHSV,lower,upper)
-        x,y=getContours(mask)
-        cv2.circle(imgOutput,(x,y),15,ColorValues[count],cv2.FILLED)
-        if x!=0 and y!=0:
-            newPoints.append([x,y,count])
-        count +=1
-        cv2.imshow(str(clr[0]),mask)   #show all the masks
-    return newPoints
-
-
-def getContours(img):  # Define the GetContours Function : Recognizes the egdes and draw a rectangular Contour
-    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-    x,y,w,h = 0,0,0,0
-    for ct in contours:
-        area = cv2.contourArea(ct)
-        if area>500:
-            cv2.drawContours(imgOutput, ct, -1, (255, 0, 0), 3)
-            peri = cv2.arcLength(ct,True)
-            approx = cv2.approxPolyDP(ct,0.02*peri,True)
-            x, y, w, h = cv2.boundingRect(approx)
-    return x+w//2,y
-
-def drawOnCam(myPoints, ColorValues):  # This function outputs the results on the live webcam screen
-    for point in myPoints:
-        cv2.circle(imgOutput, (point[0], point[1]), 10, ColorValues[point[2]], cv2.FILLED)
-
-
-while True:   # Webcam Live Video Capture Loop : (Type q to stop)
-    success, img = cap.read()
-    if img is None or (cv2.waitKey(1) & 0xFF == ord('q')):
-        break
-    imgOutput = img.copy()
-    newPoints = detectColor(img, Colors, ColorValues)
-    if len(newPoints) != 0:
-        for newP in newPoints:
-            myPoints.append(newP)
-    if len(myPoints) != 0:
-        drawOnCam(myPoints, ColorValues)
-
-    # Finally : Output the result on the screen
-    cv2.imshow("Result", imgOutput)
+if __name__ == "__main__":
+    detector = ColorDetector()
+    while True:
+        success, img = detector.cap.read()
+        if img is None or (cv2.waitKey(1) & 0xFF == ord('q')):
+            break
+        img_output = img.copy()
+        new_points = detector.detect_color(img)
+        if new_points:
+            detector.points.extend(new_points)
+        if detector.points:
+            detector.draw_on_cam()
+        cv2.imshow("Result", img_output)
